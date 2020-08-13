d3f6e65648f2731ac7a9da988291ec1f2cebc703
Leanne Dutil <ldutil@google.com>
2020-08-13 13:10:46 +0000

    Added classes and heritage

---
+++ nsis/src/nsis/instructions/Call.java
@@ -0,0 +1,16 @@
+package nsis.instructions;
+
+public class Call extends Operation{
+
+  @Override
+  public void resolveStrings() {
+    return;
+  }
+
+  @Override
+  public void resolveControlFlow() {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
